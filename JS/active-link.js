7959a3e0a66d5cf48944814067c40efd42233c07
unknown <tikhomirova23204@gmail.com>
2024-12-01 17:40:46 +0300

    lab done

--- JS/active-link.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    let pathname = document.location.pathname
-    console.log(pathname)
-    pathname = pathname.substring(pathname.lastIndexOf('/') + 1)
-    document.querySelectorAll(`[href='./${pathname}']`).forEach(x => x.classList.add('active-link'))
-})
